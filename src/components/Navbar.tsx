2cb053a06c615f8e0c9b7482941a49732ad3c4f6
Charles <creidbrowniii@gmail.com>
2025-03-01 11:19:12 -1000

    changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,78 +1,37 @@
-import {  useEffect } from "react";
 
 const Navbar = () => {
-
-    // closes navbar if you click outside of it 
-    useEffect(() => {
-        const sidebar = document.getElementById("default-sidebar");
-        const toggleButton = document.querySelector("[data-drawer-toggle='default-sidebar']");
-
-        const closeSidebar = (event: MouseEvent) => {
-            if (sidebar && toggleButton && !sidebar.contains(event.target as Node) && !toggleButton.contains(event.target as Node)) {
-                sidebar.classList.add("-translate-x-full");
-            }
-        };
-
-        document.addEventListener("click", closeSidebar);
-
-        return () => {
-            document.removeEventListener("click", closeSidebar);
-        };
-    }, []);
-
     return (
         <>
-        {/* Toggle button */}
-        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 bg-red-900 rounded-lg sm:hidden hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        <span className="sr-only">Open sidebar</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-        </svg>
-        </button>
-
-        {/* navbar */}
-        <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div className="h-full px-3 py-4 overflow-y-auto bg-red-900 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
-                <li>
-                    <a href="./" className="flex items-center p-2 text-white font-large font-bold rounded-lg group">
-                    <img src="./fhs-leo-club-logo.png" className="d-inline-block w-10 h-10 align-top shadow rounded"></img>
-                    <span className="ms-3">FARRINGTON HIGH SCHOOL LEO CLUB</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="./" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-100 ease-in-out group">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                    </svg>
-                    <span className="flex-1 ms-3 whitespace-nowrap">Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="./events.html" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-100 ease-in-out group">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                    </svg>
-                    <span className="flex-1 ms-3 whitespace-nowrap">Events</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="./contact.html" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-100 ease-in-out group">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                    </svg>
-                    <span className="flex-1 ms-3 whitespace-nowrap">Contact</span>
-                    </a>
-                </li>
-            </ul>
+        <nav className="bg-red-900 border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="./public/fhs-leo-club-logo.png" className="w-10 h-10" alt="Leo Club Logo" />
+                <span className="self-center text-2xl font-semibold text-white whitespace-nowrap dark:text-white">FHS Leo Club</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-red-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-red-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="/" className="block py-2 px-3 text-white rounded-sm hover:bg-red-800 md:bg-transparent md:text-white md:py-2 md:hover:bg-red-800 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="/events.html" className="block py-2 px-3 text-white rounded-sm hover:bg-red-800 md:hover:bg-transparent md:py-2 md:border-0 md:hover:bg-red-800 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Events</a>
+                    </li>
+                    <li>
+                        <a href="/contact.html" className="block py-2 px-3 text-white rounded-sm hover:bg-red-800 md:hover:bg-transparent md:py-2 md:border-0 md:hover:bg-red-800 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        </aside>
-
-        <div className="p-4 sm:ml-64">
-        </div>
-
-        </>
+        </nav>
+    </>
     );
+
 };
 
 export default Navbar;
