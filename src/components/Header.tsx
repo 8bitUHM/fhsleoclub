833909d1fe0278f76d0fec0e32d09f1d035c9425
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-08 08:31:46 -1000

    added a drop shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
         <>
             <div className="bg-[url('/farrington.jpg')] bg-cover bg-center h-[28rem] flex justify-center items-center text-center">
                 {/* <img src="/farrington.jpg" alt="Farrington Picture" /> */}
-                <h1 className="text-red-500 text-4xl md:text-6xl shadow-md font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
+                <h1 className="text-red-500 text-4xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
             </div>
 
         </>
