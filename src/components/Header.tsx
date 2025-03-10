3b0178d27875079085c0469f1a909981162e487b
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-09 22:38:34 -1000

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,9 @@ const Header = () => {
 
     return (
         <>
-            <div className="bg-[url('/farrington.jpg')] bg-cover bg-center h-[28rem] flex justify-center items-center text-center">
-                {/* <img src="/farrington.jpg" alt="Farrington Picture" /> */}
-                <h1 className="text-red-500 text-4xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
-            </div>
-
+            <header className="bg-[url('/farrington.jpg')] bg-cover bg-center h-[28rem] flex justify-center items-center text-center text-balance">
+                <h1 className="text-white text-5xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
+            </header>
         </>
     );
 };
