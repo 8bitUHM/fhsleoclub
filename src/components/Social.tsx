27ba40e89428fd28461851dc8837724dca5edd2b
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-14 22:08:07 -1000

    Adjusted bg color and height

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,15 +1,13 @@
 const Social = () => {
 
-    // console.log(location.pathname)
-
     return (
         <>
-            <div className="h-[12rem] bg-zinc-900 flex justify-center items-center text-balance">
+            <div className="h-[10rem] bg-red-900 flex justify-center items-center text-balance">
                 <a href="https://www.instagram.com/fhs_leo_club/" className="flex flex-row justify-center items-center">
-                    <img src="/fhs-leo-club-logo.png" className="w-20 h-20 mx-3 items-center" alt="Leo Club Logo" />
+                    <img src="/fhs-leo-club-logo.png" className="w-20 h-20 mx-3 self-center" alt="Leo Club Logo" />
                     <div className="flex flex-col">
-                        <h2 className="text-white text-4xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium py-3">Instagram @ FHS_LEO_CLUB</h2>
-                        <p className="text-gray-400 text-xl md:text-3xl">Farrington Highschool Leo Club 🦁❤️🧡</p>
+                        <h2 className="text-white text-4xl md:text-5xl drop-shadow-lg font-family font-oswald font-medium py-3">Instagram @ FHS_LEO_CLUB</h2>
+                        <p className="text-white text-xl md:text-3xl">Farrington Highschool Leo Club 🦁❤️🧡</p>
                     </div>
                 </a>
             </div>
