357f13625014d7a7f8e468afe4213d6976257db9
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-14 22:10:02 -1000

    Changed header logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <>
             <header className="bg-[url('/farrington.jpg')] bg-cover bg-center h-[28rem] flex justify-center items-center text-center text-balance">
-                <h1 className="text-white text-5xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname === "/contact/" ? "FOLLOW US ON" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
+                <h1 className="text-white text-5xl md:text-6xl drop-shadow-lg font-family font-oswald font-medium">{location.pathname === "/" ? "FARRINGTON HIGH SCHOOL LEO CLUB" : location.pathname.replace(/^\/|\/$/g, "").toUpperCase()}</h1>
             </header>
         </>
     );
