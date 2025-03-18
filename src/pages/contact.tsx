fe891b64c16200b9b4d8e30447936b90ad2fdb20
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-17 16:42:35 -1000

    Added the 3 sections

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -18,7 +18,27 @@ export function Contact() {
         <>
             <Navbar />
             <Header />
-            <Social />
+            {/* Socials Section */}
+            <section className="bg-red-900 pt-4">
+                <h2 className="text-3xl md:text-4xl font-bold text-white text-center">Stay updated with us on:</h2>
+                <Social />
+            </section>
+
+            {/* Student Section */}
+            <section className="py-4">
+                <h2 className="text-3xl md:text-4xl font-bold text-red-900 text-center">If you want to join Leo Club. Fill out the form below</h2>
+                <div className="w-full">
+                    <iframe
+                        src="https://docs.google.com/forms/d/e/1FAIpQLSd8sFRwdVTedWxaBa1aNjEWApPxMRx54vtvvnZvteWaB3IGtQ/viewform?embedded=true"
+                        className="w-full h-[34rem] md:h-[68rem] p-4">
+                    </iframe>
+                </div>
+            </section>
+
+            {/* Staff, Parents, Others section */}
+            <section className="bg-red-900 py-4">
+                <h2 className="text-3xl md:text-4xl font-bold text-white p-4 text-center break-words">For questions or collaboration, contact Leo Club at farringtonhighschoolleoclub@gmail.com</h2>
+            </section>
             <Footer />
         </>
     );
