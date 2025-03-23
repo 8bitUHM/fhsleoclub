eec931a5a93e9638b8cff4106e6828a3c22dff0f
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-22 21:51:52 -1000

    Impl: Fetching from database

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,21 +1,34 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { initFlowbite } from "flowbite";
-import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { eventRefs } from "../lib/dbRefs";
+import { ClubEvent } from "../lib/types";
+import { onValue } from "firebase/database";
 import "../index.css";
 
 export function Events() {
+    const [ events, setEvents ] = useState<Record<string, ClubEvent>>({});
+
     useEffect(() => {
         initFlowbite();
-    }, []);
 
+        const unsubscribe = onValue(eventRefs, (snapshot) => {
+            if (snapshot.exists()) {
+                setEvents(snapshot.val());
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+    
     return (
         <>
             <Navbar />
             <Header />
+            <h1>This is the events page!</h1>
             <Footer />
         </>
     );
