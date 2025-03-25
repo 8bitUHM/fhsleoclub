eab505c4fbfd386343e3009a43b2809172245b61
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-25 10:31:05 -1000

    Removed text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
 
     return (
         <>
-            <div className="inline-flex cursor-not-allowed items-center rounded-md px-4 py-2 text-xl md:text-3xl leading-6 font-semibold text-red-900 transition duration-150 ease-in-out">
+            <div className="inline-flex cursor-not-allowed items-center justify-center p-4">
                 <svg
                     className="mr-3 -ml-1 size-6 md:size-8 animate-spin text-red-900"
                     xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,6 @@ const Loading = () => {
                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                     />
                 </svg>
-                Loading...
             </div>
         </>
     );
