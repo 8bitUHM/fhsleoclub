64d49b2735f9b8b4f5e592ded65c842ca8829982
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-03-25 13:15:22 -1000

    Minor changes

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -8,7 +8,6 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { Member } from "../lib/types";
 import "../index.css";
-import Loading from "../components/Loading";
 
 
 export function Members() {
@@ -22,20 +21,15 @@ export function Members() {
     const unsubscribe = onValue(clubMembersRefs, (snapshot) => {
       if (snapshot.exists()) {
         setMembers(snapshot.val());
+        setIsLoading(false);
       } else {
         setMembers({});
       }
     });
 
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
 
     // Function call so that unsubscribe is called every time. 
-    return () => {
-      clearTimeout(timer);
-      unsubscribe();
-    };
+    return () => unsubscribe();
   }, []);
 
   const categorizeMembers = (members: Record<string, Member>) => {
@@ -59,80 +53,83 @@ export function Members() {
   };
 
   const { advisors, officers, generalMembers } = categorizeMembers(members);
+  
 
   return (
     <>
-      <Navbar />
-      <Header />
-      <div className="flex justify-center items-center">
-        {isLoading ? <Loading /> : <p>Content is loaded!</p>}
-      </div>
+      {
+        !isLoading && (
+          <>
+            <Navbar />
+            <Header />
 
-      {/* Advisors Section */}
-      <section className="bg-red-900 p-6 text-white text-center">
-        <h2 className="text-3xl md:text-4xl font-semibold pb-6">Advisors</h2>
-        <div className="flex flex-wrap justify-center items-center gap-x-11 gap-y-4">
-          {advisors.map((member) => (
-            <div key={member.email} className="mb-4 space-y-1">
-              <div className="text-lg font-medium">{member.name}</div>
-              <div className="capitalize">{member.role}</div>
-              <div>
-                <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
-                  <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-                  </svg>
-                  <span>Email</span>
-                </a>
+            {/* Advisors Section */}
+            <section className="bg-red-900 p-6 text-white text-center">
+              <h2 className="text-3xl md:text-4xl font-semibold pb-6">Advisors</h2>
+              <div className="flex flex-wrap justify-center items-center gap-x-11 gap-y-4">
+                {advisors.map((member) => (
+                  <div key={member.email} className="mb-4 space-y-1">
+                    <div className="text-lg font-medium">{member.name}</div>
+                    <div className="capitalize">{member.role}</div>
+                    <div>
+                      <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
+                        <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                        </svg>
+                        <span>Email</span>
+                      </a>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            </section>
 
-      {/* Officers Section */}
-      <section className="p-6 text-red-900 text-center">
-        <h2 className="text-3xl md:text-4xl font-semibold pb-6">Officers</h2>
-        <div className="flex flex-wrap justify-center items-center gap-x-12 gap-y-4">
-          {officers.map((member) => (
-            <div key={member.email} className="mb-4 space-y-1">
-              <div className="text-lg font-medium">{member.name}</div>
-              <div className="capitalize">{member.role}</div>
-              <div>
-                <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
-                  <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-                  </svg>
-                  <span>Email</span>
-                </a>
+            {/* Officers Section */}
+            <section className="p-6 text-red-900 text-center">
+              <h2 className="text-3xl md:text-4xl font-semibold pb-6">Officers</h2>
+              <div className="flex flex-wrap justify-center items-center gap-x-12 gap-y-4">
+                {officers.map((member) => (
+                  <div key={member.email} className="mb-4 space-y-1">
+                    <div className="text-lg font-medium">{member.name}</div>
+                    <div className="capitalize">{member.role}</div>
+                    <div>
+                      <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
+                        <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                        </svg>
+                        <span>Email</span>
+                      </a>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            </section>
 
-      {/* General Members Section */}
-      <section className="bg-red-900 p-6 text-white text-center">
-        <h2 className="text-3xl md:text-4xl font-semibold pb-6">Club Members</h2>
-        <div className="flex justify-center">
-          <div className="grid grid-flow-row-dense grid-cols-2 lg:grid-cols-4 place-items-center gap-x-8 gap-y-2">
-            {generalMembers.map((member) => (
-              <div key={member.email} className="mb-4 space-y-1">
-                <div className="text-lg font-medium">{member.name}</div>
-                <div>
-                  <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
-                    <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-                    </svg>
-                    <span>Email</span>
-                  </a>
+            {/* General Members Section */}
+            <section className="bg-red-900 p-6 text-white text-center">
+              <h2 className="text-3xl md:text-4xl font-semibold pb-6">Club Members</h2>
+              <div className="flex justify-center">
+                <div className="grid grid-flow-row-dense grid-cols-2 lg:grid-cols-4 place-items-center gap-x-8 gap-y-2">
+                  {generalMembers.map((member) => (
+                    <div key={member.email} className="mb-4 space-y-1">
+                      <div className="text-lg font-medium">{member.name}</div>
+                      <div>
+                        <a href={`mailto:${member.email}`} className="flex justify-center hover:underline">
+                          <svg className="w-8 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                          </svg>
+                          <span>Email</span>
+                        </a>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <Footer />
+            </section>
+            <Footer />
+          </>
+        )
+      }
     </>
   );
 }
