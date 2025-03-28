26d6ba5570cd8565a457972d39c83f54438467c1
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-28 11:34:59 -1000

    Moved the sign-in button to stay on the right even when on mobile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,14 +18,14 @@ const Navbar = () => {
                         <span className="self-center text-2xl font-semibold text-white whitespace-nowrap">FHS Leo Club</span>
                     </a>
 
-                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 order-3" aria-controls="navbar-default" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 order-2" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
 
-                    <div className="hidden w-full md:block md:w-auto order-3 md:order-2" id="navbar-default">
+                    <div className="hidden w-full md:block md:w-auto order-4 md:order-2" id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-red-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-red-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <a href="/" className="block py-2 px-3 text-white rounded-sm hover:bg-red-800 focus:bg-red-800 md:py-2 md:bg-transparent" aria-current="page">Home</a>
@@ -43,7 +43,7 @@ const Navbar = () => {
                         </ul>
                     </div>
 
-                    <div className="flex items-center pt-1 order-2 md:order-last space-x-0 md:space-x-3">
+                    <div className="flex items-center pt-1 order-3 space-x-0 md:space-x-3">
                         {isSignedIn ? (
                             <>
                                 <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
