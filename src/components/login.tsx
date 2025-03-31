a5c70c1ed32ca2144598459b12d74269b5cbad10
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-31 13:38:24 -1000

    commented out a section

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -22,17 +22,19 @@ const Login = () => {
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Password</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                 </div>
-                                <div className="flex items-center justify-between">
-                                    {/* <div className="flex items-start">
+
+                                {/* <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
                                         <div className="flex items-center h-5">
                                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
                                         </div>
                                         <div className="ml-3 text-sm">
                                             <label htmlFor="remember" className="text-red-900 dark:text-gray-300">Remember me</label>
                                         </div>
-                                    </div> */}
+                                    </div>
                                     <a href="#" className="text-sm font-medium text-red-900 hover:underline dark:text-primary-500">Forgot password?</a>
-                                </div>
+                                </div> */}
+
                                 <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     <a href="/" className="font-medium text-red-900 hover:underline dark:text-primary-500">Back to home</a>
