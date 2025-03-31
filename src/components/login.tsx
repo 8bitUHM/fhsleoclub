fef4c4601ccd79e3189e8f7ada409c8a86690717
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-03-31 13:56:06 -1000

    small styling fixes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,7 +2,7 @@ const Login = () => {
 
     return (
         <>
-            <section className="bg-red-900 dark:bg-gray-900">
+            <div className="bg-red-900 min-h-screen flex items-center justify-center dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-white dark:text-white">
                         <img className="w-10 h-10 mr-2" src="/fhs-leo-club-logo.png" alt="logo" />
@@ -43,7 +43,7 @@ const Login = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </>
     );
 };
