70baf952a281945be02ff866a5ae1676a4e34f52
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-04 22:35:33 -1000

    Impl: auth export

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
 export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
