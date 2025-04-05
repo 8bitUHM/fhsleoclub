3683f5b8d7159adafa05ae3acac9f3c59ceee8c6
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-04 22:41:51 -1000

    Impl: Grabbing form field data and submitting data

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -1,5 +1,31 @@
-const SignInForm = () => {
+import { ChangeEvent, FormEvent, useState } from "react";
 
+const SignInForm = () => {
+    const [ isLoading, setIsLoading ] = useState<boolean>(false);
+    const [ formFields, setFormFields ] = useState({
+        email: "",
+        password: "",
+    }); 
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        setFormFields(() => (
+            {
+                ...formFields,
+                [e.target.name]: e.target.value,
+            }
+        ));
+    }
+
+    const handleClick = (e:FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setIsLoading((prev) => !prev);
+        setTimeout(() => { 
+            console.log('doing stuff') 
+            setIsLoading((prev) => !prev);
+        }, 2000);
+    }
+
+    
     return (
         <>
             <div className="bg-red-900 min-h-screen flex items-center justify-center">
@@ -16,11 +42,11 @@ const SignInForm = () => {
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-red-900">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="School Email" required />
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="School Email" required onChange={handleChange}/>
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-red-900">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required onChange={handleChange}/>
                                 </div>
 
                                 <div className="flex flex-col gap-2">
@@ -28,7 +54,7 @@ const SignInForm = () => {
                                     <a href="/signup/" className="text-sm font-medium text-red-900 hover:underline">Don't have an account? Sign up here</a>
                                 </div>
 
-                                <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                                <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-not-allowed disabled:bg-red-500" onClick={handleClick} disabled={isLoading} aria-disabled={isLoading}>Sign in</button>
 
                                 <a href="/" className="font-medium text-red-900 text-sm block hover:underline">Back to home</a>
                             </form>
