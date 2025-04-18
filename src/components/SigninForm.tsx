1a9165bf2ebbb8a865206ca96773b7b4c010be01
Benny <76574420+bennytrieu@users.noreply.github.com>
2025-04-17 14:01:24 -1000

    added error message popup

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -27,7 +27,7 @@ const SignInForm = () => {
             await signInWithEmailAndPassword(auth, email, password);
             window.location.href = "/";
         } catch (err: any) {
-            alert(err);
+            // alert(err);
             setShowMessage(true);
             switch (err.code) {
                 case "auth/invalid-email":
@@ -77,10 +77,6 @@ const SignInForm = () => {
                                     <a href="/signup/" className="text-sm font-medium text-red-900 hover:underline">Don't have an account? Sign up here</a>
                                 </div>
 
-                                <div className={showMessage ? '' : 'hidden'}>
-                                    <p className="text-sm font-medium text-center text-red-500">{message}</p>
-                                </div>
-
                                 <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-progress disabled:bg-red-500" onClick={handleClick} disabled={isLoading} aria-disabled={isLoading}>Sign in</button>
 
                                 <a href="/" className="font-medium text-red-900 text-sm block hover:underline">Back to home</a>
@@ -89,6 +85,31 @@ const SignInForm = () => {
                     </div>
                 </div>
             </div>
+
+            {showMessage && (
+                <div tabIndex={-1} className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div className="relative p-4 w-full max-w-md max-h-full">
+                        <div className="relative border-2 border-black bg-white rounded-lg shadow-sm">
+                            <button type="button" className="absolute top-3 end-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                                onClick={() => setShowMessage(false)}
+                            >
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                            <div className="p-4 md:p-5 text-center">
+                                <svg className="mx-auto mb-4 text-red-600 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <h3 className="mb-5 text-lg font-normal text-red-600">{message}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
         </>
     );
 };
