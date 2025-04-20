cd296dd50463ba19cc86da5af39999013482656e
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-19 23:01:59 -1000

    Got rid of lint error

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { auth } from "../lib/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { AuthError } from "firebase/auth";
 
 const SignInForm = () => {
     const [message, setMessage] = useState("Test error message");
@@ -26,16 +27,15 @@ const SignInForm = () => {
         try {
             await signInWithEmailAndPassword(auth, email, password);
             window.location.href = "/";
-        } catch (err: any) {
-            // alert(err);
+        } catch (err) {
             setShowMessage(true);
-            switch (err.code) {
+            switch ((err as AuthError).code) {
                 case "auth/invalid-email":
                     if (email.length === 0 || password.length === 0) {
                         setShowMessage(true);
                         setMessage("Please fill out all information");
                     } else {
-                        setMessage("It must be a valid email");
+                        setMessage("Please enter a valid email");
                     }
                     break;
                 case "auth/invalid-credential":
@@ -77,7 +77,7 @@ const SignInForm = () => {
                                     <a href="/signup/" className="text-sm font-medium text-red-900 hover:underline">Don't have an account? Sign up here</a>
                                 </div>
 
-                                <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-progress disabled:bg-red-500" onClick={handleClick} disabled={isLoading} aria-disabled={isLoading}>Sign in</button>
+                                <button type="submit" className="w-full text-white bg-red-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-progress disabled:bg-red-500" onClick={handleClick} disabled={isLoading || showMessage} aria-disabled={isLoading || showMessage}>Sign in</button>
 
                                 <a href="/" className="font-medium text-red-900 text-sm block hover:underline">Back to home</a>
                             </form>
@@ -87,12 +87,12 @@ const SignInForm = () => {
             </div>
 
             {showMessage && (
-                <div tabIndex={-1} className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                     <div className="relative p-4 w-full max-w-md max-h-full">
                         <div className="relative border-2 border-black bg-white rounded-lg shadow-sm">
                             <button type="button" className="absolute top-3 end-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                                 onClick={() => setShowMessage(false)}
-                            >
+                                >
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
