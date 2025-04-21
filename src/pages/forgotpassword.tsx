9a8713912953e68b42ff3ff1d26cecd12a18e74c
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-20 20:39:36 -1000

    Impl: Getting values in form

--- src/pages/forgotpassword.tsx
+++ src/pages/forgotpassword.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect } from "react";
+import { FormEvent, StrictMode, useEffect, useRef } from "react";
 import { initFlowbite } from "flowbite";
 import { createRoot } from "react-dom/client";
 
@@ -8,6 +8,13 @@ export function ForgotPassword() {
   useEffect(() => {
     initFlowbite();
   }, []);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    alert(emailRef.current!.value);
+  };
 
   return (
     <>
@@ -40,11 +47,13 @@ export function ForgotPassword() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="Your email"
                   required
+                  ref={emailRef}
                 />
               </div>
               <button
                 type="submit"
                 className="w-full text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                onClick={handleClick}
               >
                 Reset Password
               </button>
