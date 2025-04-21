719ab595d604104cda54882eccfcb7fd9089a732
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-20 23:24:13 -1000

    Added modal for showing success or error messages

--- src/pages/forgotpassword.tsx
+++ src/pages/forgotpassword.tsx
@@ -1,25 +1,40 @@
-import { FormEvent, StrictMode, useEffect, useRef } from "react";
+import { FormEvent, StrictMode, useEffect, useRef, useState } from "react";
 import { initFlowbite } from "flowbite";
 import { createRoot } from "react-dom/client";
-
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../lib/config";
 import "../index.css";
 
 export function ForgotPassword() {
   useEffect(() => {
     initFlowbite();
-  }, []);
+  });
 
   const emailRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string>("");
+  // Checks for valid email patterns
+  const regex = /^[\w.-]+@\w+(?:\.\w+){1,2}$/;
 
-  const handleClick = (e: FormEvent<HTMLButtonElement>) => {
+  const handleClick = async (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    alert(emailRef.current!.value);
+    if (!emailRef.current!.value) {
+      setError("Please fill out email");
+    } else if (!regex.test(emailRef.current!.value)) {
+      setError("Please enter a valid email");
+    } else {
+      setError("");
+      try {
+        alert("Doing stuff");
+      } catch (e) {
+        setError((e as Error).message);
+      }
+    }
   };
 
   return (
     <>
       <main className="bg-red-900 min-h-screen min-w-screen text-white flex flex-col justify-center items-center">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 max-w-[50ch]">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 max-w-[50ch]">
           <a href="/" className="flex items-center mb-6 text-2xl font-bold">
             <img
               className="w-10 h-10 mr-2"
@@ -54,17 +69,79 @@ export function ForgotPassword() {
                 type="submit"
                 className="w-full text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 onClick={handleClick}
+                data-modal-target="modal"
+                data-modal-show="modal"
               >
                 Reset Password
               </button>
               <section className="text-sm font-medium text-red-900 space-y-2 *:block">
-                <a href="/signup/">Sign Up</a>
-                <a href="/">Back to Home Page</a>
+                <a href="/signin/" className="hover:underline">
+                  Sign In
+                </a>
+                <a href="/" className="hover:underline">
+                  Back to Home Page
+                </a>
               </section>
             </form>
           </div>
         </div>
       </main>
+      <dialog
+        id="modal"
+        className="hidden min-w-full min-h-full top-0 bg-black/75 md:p-0"
+      >
+        {/* Modal for Success or Error Messages */}
+        {!error ? (
+          <section className="max-w-[35ch] md:max-w-[40ch] bg-white p-4 rounded-lg flex flex-col justify-center gap-4">
+            <h1 className="text-2xl font-bold text-red-800">Mahalo Nui Loa!</h1>
+            <p className="text-sm text-gray-500">
+              We will send an email to the one entered if it exists in our
+              system. Please note that the email may take some time to send. If
+              you do not receive the email, please make sure to check your
+              Junk/Spam Folder
+            </p>
+            <a
+              href="/signin/"
+              data-modal-hide="modal"
+              className="p-2 bg-red-700 rounded-lg text-white text-sm font-semibold tracking-wide focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-red-600 text-center"
+            >
+              Take Me to Sign In Page
+            </a>
+          </section>
+        ) : (
+          <section className="max-w-[35ch] md:max-w-[40ch] bg-white p-4 rounded-lg flex flex-col justify-center gap-4">
+            <h1 className="text-2xl font-bold text-red-500 inline-flex items-center gap-1">
+              <svg
+                className="w-9 h-9 text-red-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              Error Encountered
+            </h1>
+            <p className="text-sm text-red-900">{error}</p>
+            <button
+              type="button"
+              data-modal-target="modal"
+              data-modal-hide="modal"
+              className="p-2 bg-red-700 rounded-lg text-white text-sm font-semibold tracking-wide focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-red-600 text-center"
+            >
+              Close
+            </button>
+          </section>
+        )}
+      </dialog>
     </>
   );
 }
