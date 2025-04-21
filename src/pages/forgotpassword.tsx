78773dc3e74b7b7b40a48e9368633df8e6f18fa4
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-20 20:12:44 -1000

    Created form

--- src/pages/forgotpassword.tsx
+++ src/pages/forgotpassword.tsx
@@ -4,20 +4,64 @@ import { createRoot } from "react-dom/client";
 
 import "../index.css";
 
-
 export function ForgotPassword() {
-    useEffect(() => {
-        initFlowbite();
-    }, []);
+  useEffect(() => {
+    initFlowbite();
+  }, []);
 
-
-    return(
-        <h1>Hello World</h1>
-    );
+  return (
+    <>
+      <main className="bg-red-900 min-h-screen min-w-screen text-white flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 max-w-[50ch]">
+          <a href="/" className="flex items-center mb-6 text-2xl font-bold">
+            <img
+              className="w-10 h-10 mr-2"
+              src="/fhs-leo-club-logo.png"
+              alt="logo"
+            />
+            FHS Leo Club
+          </a>
+          <div className="w-full p-6 bg-white rounded-lg shadow md:mt-0 sm:max-w-md sm:p-8">
+            <h2 className="mb-1 text-xl font-bold leading-tight text-red-900 md:text-2xl">
+              Password Reset via Email
+            </h2>
+            <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-red-800"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="Your email"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Reset Password
+              </button>
+              <section className="text-sm font-medium text-red-900 space-y-2 *:block">
+                <a href="/signup/">Sign Up</a>
+                <a href="/">Back to Home Page</a>
+              </section>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <ForgotPassword />
-    </StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ForgotPassword />
+  </StrictMode>
 );
