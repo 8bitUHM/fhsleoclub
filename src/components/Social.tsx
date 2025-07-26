164310a23cdd3d7ffec4cb9e927132714ecc692a
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-07-25 14:45:48 -1000

    Added new links and verified they work

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,10 +3,10 @@ const Social = () => {
     return (
         <>
             <div className="h-[10rem] bg-red-900 flex justify-center items-center text-balance">
-                <a href="https://www.instagram.com/fhs_leo_club/" className="flex flex-row justify-center items-center">
+                <a href="https://www.instagram.com/farringtonhs_leo_club/" className="flex flex-row justify-center items-center">
                     <img src="/fhs-leo-club-logo.png" className="w-20 h-20 mx-3 self-center" alt="Leo Club Logo" />
                     <div className="flex flex-col">
-                        <h2 className="text-white text-4xl md:text-5xl drop-shadow-lg font-family font-oswald font-medium py-3">Instagram @ FHS_LEO_CLUB</h2>
+                        <h2 className="text-white text-4xl md:text-5xl drop-shadow-lg font-family font-oswald font-medium py-3">Instagram @ farringtonhs_leo_club</h2>
                         <p className="text-white text-xl md:text-3xl">Farrington Highschool Leo Club 🦁❤️🧡</p>
                     </div>
                 </a>
